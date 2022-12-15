a6cf03ca5dcd7f4d59571c6061959d55f9edcfbb
Nguyễn Bá Duy <43690005+duynguyen97@users.noreply.github.com>
2022-12-15 16:00:50 +0700

    icon component

---
+++ components/Icon/Icon.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Icon } from "./Icon";
+
+export default {
+  title: "Content/Icon",
+  component: Icon,
+} as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+
+export const BasicIcon = Template.bind({});
+BasicIcon.args = {
+  name: "User",
+};
